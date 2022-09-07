bb3e1ca0793d382eea479a232551c4158bd8a099
ayushman-kaushal <72315694+ayushman-kaushal@users.noreply.github.com>
2022-09-07 23:39:42 +0530

    Update mergeSortedArray1.cpp

--- Lecture020 ArrayQuestions/mergeSortedArray1.cpp
+++ Lecture020 ArrayQuestions/mergeSortedArray1.cpp
@@ -22,7 +22,7 @@ void merge(int arr1[], int n, int arr2[], int m, int arr3[]) {
 
     //copy kardo second array k remaining element ko
     while(j<m) {
-        arr2[k++] = arr2[j++];
+        arr3[k++] = arr2[j++];  //arr2[k++] = arr2[j++]; Error detected at this place Now it's working fine bhaiya
     }
 }
 
